ed21f623571212370f9c27e3d563c42364db7f0d
SUMEDH055 <100142281+SUMEDH055@users.noreply.github.com>
2024-04-15 21:34:44 +0530

    Update string.js

--- string.js
+++ string.js
@@ -9,14 +9,12 @@ console.log(friend)
 let myfriend = "Shubham"
 console.log(myfriend)
 
-// string 2 Template literals
+// string 2 
 let boy1 = "Sumedh"
 let boy2 = "ved"
 let sentence = `${boy2} is a friend of ${boy1}`    // The output should be Sumedh is a friend of ved
 console.log(sentence)
 
-// Escape Sequence Characters
-// let fruit = 'Bana\'na'
-// console.log(fruit)
+
 let fruit = "Bana\"na"
 console.log(fruit)
